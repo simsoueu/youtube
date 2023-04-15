ac6b4c54a710ba3c66575dbc2064de447346b07d
simsoueu <joaoapenas@gmail.com>
2023-04-14 23:22:29 -0300

    created the history

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, CommonModule]
 })
 export class Tab2Page {
+  templates: any[] = [];
+  showData: boolean = false;
 
-  constructor() {}
+  constructor() {
+    this.templates = [
+      'BTB | Full Body (2)',
+      'Upper / Lower Body Split (4)',
+      'Bodybuilding Bro Split (6)',
+      'Push / Pull / Legs (3)',
+      'Complete Workout (3)',
+      'Go to the gym to take pics (3)',
+      'Only arms (2)',
+      'Soccer bro (4)',
+      'PHUL Training Program (4)',
+      'BTB | Full Body (3)',
+      'Upper / Lower Body Split (6)',
+    ].map((val) => ({
+      title: val, selected: false,
+    }));
+
+    setTimeout(() => {
+      this.selectSegment(0);
+      this.showData = true;
+      // this.videos = homeData;
+    }, 1500)
+
+  }
+
+  selectSegment(index: any) {
+    this.templates.map(item => item.selected = false);
+    this.templates[index].selected = true;
+  }
 
 }
